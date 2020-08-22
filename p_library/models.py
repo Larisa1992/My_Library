742b551eede86860948d1e976efc182854d06ca9
larisa1992 <pelar1992@mail.ru>
2020-08-22 22:38:05 +0300

    initial commit

---
+++ p_library/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Author(models.Model):  
+    full_name = models.TextField()  
+    birth_year = models.SmallIntegerField()  
+    country = models.CharField(max_length=2)  
+  
+    def __str__(self):  
+        return self.full_name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    authors = models.ManyToManyField(
+        Author,
+        through='Inspiration',
+        through_fields=('book', 'author'),
+    )
+
+# вдохновитель inspirer для автора author при написании книги book
+class Inspiration(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    inspirer = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="inspired_works",
+    )
